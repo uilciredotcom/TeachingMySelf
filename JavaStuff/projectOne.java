398384332a4e645fbc50eb28859008a82a93fefb
uilciredotcom <54650161+uilciredotcom@users.noreply.github.com>
2019-08-31 20:09:35 -0400

    Add:testingjava and Website Html

--- JavaStuff/projectOne.java
+++ JavaStuff/projectOne.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class projectOne {
     public static void main(String []args) {
         System.out.println("Hello everyone! This is just a test.");
+        
+        for (int x = 0; x <= 10; x++ ){
+            System.out.println(x);
+        }
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your favorite color?");
+        String answer = input.next();
+
+        System.out.println("You favorite color is " + answer);
+        
+        
+    
+            
+        
+    
     }
     
 }
